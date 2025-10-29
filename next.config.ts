562d3b9e76c2f5fe5631f556ffadc590a575f57c
Jen <jen_ben@mit.edu>
2025-10-28 22:13:22 -0400

    added images hosted

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/dv8rcbg2n/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
