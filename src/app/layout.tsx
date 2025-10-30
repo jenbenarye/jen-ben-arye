23c75c51a33cbb19cc79c5881ec5d7ec4561f9ac
Jen <jen_ben@mit.edu>
2025-10-30 16:04:50 -0400

    added socials metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,13 +12,27 @@ const jetbrainsMono = JetBrains_Mono({
 
 export const metadata: Metadata = {
   title: "Jen Ben Arye",
-  description: "My corner of the web.",
+  description: "My corner of the web",
   icons: {
-    icon: [
-      { url: "/site-icon.png", type: "image/png", sizes: "250x250" },
-    ],
+    icon: { url: "/site-icon.png", type: "image/png", sizes: "250x250" },
   },
+  openGraph: {
+    type: "website",
+    url: "https://www.jenbenarye.com/",
+    title: "Jen Ben Arye",
+    description: "My corner of the web",
+    siteName: "Jen Ben Arye",
+    images: [{ url: "/site-icon-1000.png" }]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jen Ben Arye",
+    description: "My corner of the web",
+    images: "/site-icon-1000.png"
+  }
 };
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
